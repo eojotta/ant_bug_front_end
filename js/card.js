a7b7aa2abbeac66d56983e4bb3e2c17d68ffbf1a
ESTELA RUFINO BRITO <25203670@dsn127m1241953.cfp127aluno.local>
2026-06-17 15:01:58 -0300

    recebendo atualizacao

--- js/card.js
+++ js/card.js
@@ -1,1 +1,32 @@
 import { buscarDadosApi } from "./app";
+
+function criarCards(url){
+    const url = buscarDadosApi()
+
+const main = document.getElementById('main')
+
+    const conteiner = document.createElement('div')
+    conteiner.className = 'conteinerCards'
+        
+    const titulo = document.createElement('h2')
+    titulo.className = 'titulo' 
+    titulo.textContent = url.name
+
+    const imgProduto = document.createElement('img')
+    imgProduto.src = url.image.medium
+    imgProduto.className = 'imgProduto'
+
+    const miniInformacao = document.createElement('p')
+    miniInformacao.textContent = url.language
+
+    const bntSaibaMais = document.createElement('button')
+    bntSaibaMais.className = 'saibaMais'
+    bntSaibaMais.textContent = "Saiba Mais"
+
+    const subCategoria = document.createElement('p')
+    subCategoria.textContent = url.status
+
+    conteiner.append(titulo,imgProduto,miniInformacao,bntSaibaMais,subCategoria)
+    main.appendChild()
+    return main
+}
